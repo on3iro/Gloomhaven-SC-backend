af2e3569ec16333d409efb81f5ad1b916bb899fd
Theo Salzmann <dev@on3iro.de>
2017-02-02 20:08:09 +0100

    Added indices

--- database_init.js
+++ database_init.js
@@ -3,22 +3,13 @@ let r = require('rethinkdb');
 function createTables(connection, db) {
   return Promise.all(
     [
-      db.tableCreate('Scenarios').run(connection),
-      db.tableCreate('Maps').run(connection),
-      db.tableCreate('MapAssets').run(connection),
-      db.tableCreate('Sections').run(connection),
-      db.tableCreate('Requirements').run(connection),
-      db.tableCreate('Goals').run(connection),
-      db.tableCreate('ScenarioComments').run(connection),
-      db.tableCreate('Assets').run(connection),
-      db.tableCreate('AssetFields').run(connection),
       db.tableCreate('AssetTypes').run(connection),
-      db.tableCreate('AssetTypeStats').run(connection),
-      db.tableCreate('StatFields').run(connection),
-      db.tableCreate('Campaigns').run(connection),
-      db.tableCreate('CampaignScenarios').run(connection),
-      db.tableCreate('CampaignScenarioLinks').run(connection),
+      db.tableCreate('Assets').run(connection),
       db.tableCreate('CampaignComments').run(connection),
+      db.tableCreate('Campaigns').run(connection),
+      db.tableCreate('Maps').run(connection),
+      db.tableCreate('ScenarioComments').run(connection),
+      db.tableCreate('Scenarios').run(connection),
       db.tableCreate('Users').run(connection),
     ]
   );
@@ -27,50 +18,54 @@ function createTables(connection, db) {
 function createInitialData(connection, db) {
   return Promise.all(
     [
-      db.table('AssetFields').insert({
-        value: "5|7"
-      }).run(connection),
-      db.table('AssetTypeStats').insert({
-      }).run(connection),
       db.table('AssetTypes').insert({
+        title: 'Enemy',
+        icon: null,
+        fields: [
+          {name: 'attack'},
+          {name: 'defense'},
+          {name: 'life'},
+        ],
       }).run(connection),
       db.table('Assets').insert({
         createdAt: r.now(),
-        title: 'Soldier',
+        creatorID: '',
+        typeID: '',
+        title: 'Crazy Wolf',
         public: 'false',
       }).run(connection),
       db.table('CampaignComments').insert({
         createdAt: r.now(),
+        campaignID: '',
+        userID: '',
         text: 'This campaign is awesome'
-      }).run(connection),
-      db.table('CampaignScenarioLinks').insert({
-      }).run(connection),
-      db.table('CampaignScenarios').insert({
-        scenarioCode: 'Q1',
       }).run(connection),
       db.table('Campaigns').insert({
         createdAt: r.now(),
+        creatorID: '',
         title: 'The awesome dummy campaign',
         description: 'Only true adventures know how to deal with "The Dummy"...',
         introduction: 'Once upon a time...',
         conclusion: '...and "The Dummy" said:...',
         rating: 5,
-        public: true
-      }).run(connection),
-      db.table('Goals').insert({
-        text: 'Kill all enemies',
-      }).run(connection),
-      db.table('MapAssets').insert({
-        playerCount: 3,
-        fieldCoordinate: '23',
+        public: true,
+        scenarios: [
+          {id: '', scenarioCode: 'Q1'}
+        ],
+        links: [
+          {sourceID: '', source: 'Q1', targetID: '', target: 'Q2'}
+        ],
       }).run(connection),
       db.table('Maps').insert({
-      }).run(connection),
-      db.table('Requirements').insert({
-        text: 'none',
+        scenarioID: '',
+        assets: [
+          {assetID: '', playerCount: 3, fieldCoordinate: '23'}
+        ],
       }).run(connection),
       db.table('ScenarioComments').insert({
         createdAt: r.now(),
+        scenarioID: '',
+        userID: '',
         text: "Wow, what a scenario...",
       }).run(connection),
       db.table('Scenarios').insert({
@@ -82,12 +77,16 @@ function createInitialData(connection, db) {
         description: "Crazy wolf slaying",
         rating: 2,
         public: true,
-      }).run(connection),
-      db.table('Sections').insert({
-        text: 'Yadayada',
-      }).run(connection),
-      db.table('StatFields').insert({
-        title: 'Attack',
+        sections: [
+          {title: 'Section One', content: 'Yadayadayada'}
+        ],
+        goals: [
+          {goal: 'Kill all enemies'},
+          {goal: 'Have fun'},
+        ],
+        requirements: [
+          {requirement: 'Be cool'},
+        ],
       }).run(connection),
       db.table('Users').insert({
         name: 'Dude',
@@ -99,11 +98,53 @@ function createInitialData(connection, db) {
   );
 }
 
-function createRelations(connection, db) {
+function createIndices(connection, db) {
   return Promise.all([
-    console.log('reeeelations')
+    db.table('Assets').indexCreate('creatorID').run(connection)
+      .then(
+        val => console.log(val)
+      ),
+    db.table('Assets').indexCreate('typeID').run(connection)
+      .then(
+        val => console.log(val)
+      ),
+    db.table('CampaignComments').indexCreate('campaignID').run(connection)
+      .then(
+        val => console.log(val)
+      ),
+    db.table('CampaignComments').indexCreate('userID').run(connection)
+      .then(
+        val => console.log(val)
+      ),
+    db.table('Campaigns').indexCreate('creatorID').run(connection)
+      .then(
+        val => console.log(val)
+      ),
+    db.table('Maps').indexCreate('scenarioID').run(connection)
+      .then(
+        val => console.log(val)
+      ),
+    db.table('ScenarioComments').indexCreate('scenarioID').run(connection)
+      .then(
+        val => console.log(val)
+      ),
+    db.table('ScenarioComments').indexCreate('userID').run(connection)
+      .then(
+        val => console.log(val)
+      ),
   ]);
 }
+
+function createRelations(connection, db) {
+  return Promise.all([
+
+  ]);
+}
+
+
+// -----------------------------------------------------------------------------
+// Query execution
+// -----------------------------------------------------------------------------
 
 // Establish connection
 let connection = null;
@@ -133,6 +174,7 @@ r.connect( {host: 'localhost', port: 28015}, (err, conn) => {
     return new Promise(
       (resolve, reject) => {
         console.log(value);
+
         // Create Database
         r.dbCreate('gloomhavenSC').run(connection, (err, result) => {
           if(err) throw err;
@@ -173,7 +215,19 @@ r.connect( {host: 'localhost', port: 28015}, (err, conn) => {
     return new Promise(
       (resolve, reject) => {
         console.log(obj.values);
-        createRelations(connection, obj.db);
+
+        createIndices(connection, obj.db).then(
+          () => resolve(obj.db)
+        );
+      }
+    )
+  }
+).then(
+  (db) => {
+    return new Promise(
+      (resolve, reject) => {
+
+        createRelations(connection, db);
       }
     )
   }

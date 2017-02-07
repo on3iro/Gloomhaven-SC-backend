3e341ac80daf6c6de496704ba29c508f8ea6eb99
Theo Salzmann <dev@on3iro.de>
2017-02-07 14:57:52 +0100

    Changed npm start and dev script and toyed around with a few things..

--- src/app.js
+++ src/app.js
@@ -3,10 +3,21 @@ import logger from 'morgan';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import helmet from 'helmet';
+import passport from 'passport';
+import { LocalStrategy } from 'passport-local';
+import { ExtractJWT } from 'passport-jwt';
+import { JwtStrategy } from 'passport-jwt';
 
+import { User } from './users/models';
 import users from './users/api';
 
 const app = express();
+
+// Define passport
+// const localOptions = { usernameField: 'mail'};
+// const localLogin = new LocalStrategy(localOptions, (email, password, done) => {
+// });
+
 
 // Initialize modules
 app.use(logger('dev'));
@@ -17,14 +28,20 @@ app.use(helmet());
 
 // Initialize API
 app.get('/', (req, resp) => {
-  resp.send('Hello World');
+  User.filter({mail: 'foo@bar.com'}).run().then(val => resp.send(val));
+  // resp.send('test');
 });
 app.use('/users', users);
 
 // Error handling
-app.use((error, request, response, next) => {
-  response.status(error.status || 500);
-  response.json({ error: error.message });
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: error.message });
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Acces-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
 });
 
 const server = app.listen(3000, () => {
